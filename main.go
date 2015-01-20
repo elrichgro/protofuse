2a1e03199924140b5800fbb7d3224e87fcde7c8a
Elrich Groenewald <elrgro@gmail.com>
2015-01-20 10:01:48 +0200

    Updated licencing

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
-// Mount marshalled protocol buffers as a FUSE filesystem.
-// command line arguments:
-//		mount location
-//		marshalled protocol buffer
-//		descriptor .proto file
-// 		message name
+//  Copyright 2015 Elrich Groenewald
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 package main
 
